0b3707b193062ba4ac15ae0644c330465a77b567
Etsetro <ronalds.palacis@outlook.com>
2020-10-19 13:20:01 +0300

    Implemented dynamic chart

--- components/Simulation.jsx
+++ components/Simulation.jsx
@@ -1,12 +1,21 @@
 import styles from "../styles/simulation.module.css";
 import Popup from "./Popup";
 import { Line } from "react-chartjs-2";
-import { useState } from "react";
 
 export default function Simulation({ values, isOpen, setIsOpen, chartData }) {
   return (
     <section className={styles.simulation}>
-      <Line data={chartData}></Line>
+      <Line
+        data={chartData}
+        options={{
+          responsive: true,
+          title: { text: "Emission in selected time period", display: true },
+          animation: {
+            duration: 500,
+            easing: "linear",
+          },
+        }}
+      />
       {!isNaN(values.gMiYear) && (
         <button
           className={styles["btn-submit"]}
