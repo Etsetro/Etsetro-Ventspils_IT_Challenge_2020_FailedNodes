8adade887f7d08c44b5df4f7a142689c07d8f90f
Etsetro <ronalds.palacis@outlook.com>
2020-10-29 23:38:20 +0200

    Navbar overlay fix test

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 
 export default function Navbar() {
   const router = useRouter();
+  let className;
   if (router.pathname === "/about") {
     className = `${styles.active}`;
   } else if (router.pathname === "/") {
@@ -11,22 +12,20 @@ export default function Navbar() {
   }
 
   return (
-    <div className={styles["navbar-container"]}>
-      <nav className={styles.navbar}>
-        <ul>
-          <Link href="/">
-            <a className={router.pathname === "/" ? styles.active : ""}>
-              Simulation
-            </a>
-          </Link>
-          <Link href="about">
-            <a className={router.pathname === "/about" ? styles.active : ""}>
-              About us
-            </a>
-          </Link>
-        </ul>
-        <p>Developed within Ventspils IT challenge 2020</p>
-      </nav>
-    </div>
+    <nav className={styles.navbar}>
+      <ul>
+        <Link href="/">
+          <a className={router.pathname === "/" ? styles.active : ""}>
+            Simulation
+          </a>
+        </Link>
+        <Link href="about">
+          <a className={router.pathname === "/about" ? styles.active : ""}>
+            About us
+          </a>
+        </Link>
+      </ul>
+      <p>Developed within Ventspils IT challenge 2020</p>
+    </nav>
   );
 }
