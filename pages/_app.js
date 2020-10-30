f6571b8107aed3c05cf06bc6a19d39ed0c691549
Etsetro <ronalds.palacis@outlook.com>
2020-10-30 21:15:19 +0200

    Bug fixes No.2

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
-import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [scrollPos, setScrollPos] = useState(0);
   return (
     <div className="app">
-      <Navbar setScrollPos={setScrollPos} />
-      <Component {...pageProps} scrollPos={scrollPos} />
+      <Navbar />
+      <Component {...pageProps} />
       <p className="info">Developed within Ventspils IT challenge 2020</p>
     </div>
   );
